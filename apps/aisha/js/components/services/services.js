aeb06f35b70aaef77ab4db5e7c05efcef8e5ee1b
duyanh <duyanh2005@gmail.com>
2017-02-02 19:10:03 +0700

    - put aisha content & images

--- apps/aisha/js/components/services/services.js
+++ apps/aisha/js/components/services/services.js
@@ -1,59 +1,121 @@
 ﻿(function () {
     'use strict';
-    angular.module('aisha.services', ['aisha.common'])
-    .factory('baseService', ['$http', '$q', function ($http, $q) {
-		// constructor
-        var baseService = function () {
-			//TO DO: review code BaseService & merge code inheritance
-        };
-
-        // methods
-        baseService.prototype.getData = function (siteUrl, listTitle) {
-            var url = String.format("{0}/_api/web/lists/getbytitle('{1}')/items", siteUrl, listTitle);
-
-            var q = $q.defer();
-            $http({
-                url: url,
-                method: 'GET',
-                headers: {
-                    "Content-Type": "application/json;odata=verbose",
-                    "Accept": "application/json;odata=verbose"
-                }
-            }).success(function (result) {
-                q.resolve(result);
-            }).error(function (error, status) {
-                q.reject(error);
-            });
-            return q.promise;
-        };
-
-        return new baseService();
-    }])
+    angular.module('aisha.services', ['aisha.common'])    
     .factory('aishaService', ['$http', '$q', 'appCommon', function ($http, $q, appCommon) {
         // constructor
         var aishaService = function () {
-            //this._siteUrl = _spPageContextInfo.webAbsoluteUrl;
             this._siteUrl = '';
-            this._digest = $("#__REQUESTDIGEST").val();
-        };                                
+        };
 
-        aishaService.prototype.getItemById = function (siteUrl, listTitle, itemId) {                
-            var listGuid = appCommon.getListIdByListTitle(listTitle);
-            var url = String.format("{0}/_api/web/lists(guid'{1}')/items?$filter=Id eq {2}", siteUrl, listGuid, itemId);
-
+        aishaService.prototype.getHeader = function () {
             var q = $q.defer();
-            $http({
-                url: url,
-                method: 'GET',
-                headers: {
-                    "Content-Type": "application/json;odata=verbose",
-                    "Accept": "application/json;odata=verbose"
+            var data = {
+                slogan: 'We are here to create @mazing things.',
+                description: 'It’s not only a Mission Impossible in the movies. </br> In real life, we always try make a mission possible done.',
+                submit: {
+                    title: 'START  TODAY  with  us',
+                    link: '#'
                 }
-            }).success(function (result) {
-                q.resolve(result.d.results[0]);
-            }).error(function (error, status) {
-                q.reject(error);
-            });
+            };
+            q.resolve(data);
+            return q.promise;
+        };
+
+        aishaService.prototype.getFeature = function () {
+            var q = $q.defer();
+            var data = {
+                slogan: 'We are here to create @mazing things.',
+                description: 'It’s not only a Mission Impossible in the movies. </br> In real life, we always try make a mission possible done.',
+                submit: {
+                    title: 'START  TODAY  with  us',
+                    link: '#'
+                }
+            };
+            q.resolve(data);
+            return q.promise;
+        };
+
+        aishaService.prototype.getStory = function () {
+            var q = $q.defer();
+            var data = {
+                slogan: 'We are here to create @mazing things.',
+                description: 'It’s not only a Mission Impossible in the movies. </br> In real life, we always try make a mission possible done.',
+                submit: {
+                    title: 'START  TODAY  with  us',
+                    link: '#'
+                }
+            };
+            q.resolve(data);
+            return q.promise;
+        };
+
+        aishaService.prototype.getClientSay = function () {
+            var q = $q.defer();
+            var data = {
+                slogan: 'We are here to create @mazing things.',
+                description: 'It’s not only a Mission Impossible in the movies. </br> In real life, we always try make a mission possible done.',
+                submit: {
+                    title: 'START  TODAY  with  us',
+                    link: '#'
+                }
+            };
+            q.resolve(data);
+            return q.promise;
+        };
+
+        aishaService.prototype.getExperience = function () {
+            var q = $q.defer();
+            var data = {
+                slogan: 'We are here to create @mazing things.',
+                description: 'It’s not only a Mission Impossible in the movies. </br> In real life, we always try make a mission possible done.',
+                submit: {
+                    title: 'START  TODAY  with  us',
+                    link: '#'
+                }
+            };
+            q.resolve(data);
+            return q.promise;
+        };
+
+        aishaService.prototype.getBlogs = function () {
+            var q = $q.defer();
+            var data = {
+                slogan: 'We are here to create @mazing things.',
+                description: 'It’s not only a Mission Impossible in the movies. </br> In real life, we always try make a mission possible done.',
+                submit: {
+                    title: 'START  TODAY  with  us',
+                    link: '#'
+                }
+            };
+            q.resolve(data);
+            return q.promise;
+        };
+
+        aishaService.prototype.getActions = function () {
+            var q = $q.defer();
+            var data = {
+                slogan: 'We are here to create @mazing things.',
+                description: 'It’s not only a Mission Impossible in the movies. </br> In real life, we always try make a mission possible done.',
+                submit: {
+                    title: 'START  TODAY  with  us',
+                    link: '#'
+                }
+            };
+            q.resolve(data);
+            return q.promise;
+        };
+
+        aishaService.prototype.getFooter = function () {
+            var q = $q.defer();
+            var data = {
+                slogan: 'We are here to create @mazing things.',
+                description: 'It’s not only a Mission Impossible in the movies. </br> In real life, we always try make a mission possible done.',
+                submit: {
+                    title: 'START  TODAY  with  us',
+                    link: '#'
+                }
+            };
+            q.resolve(data);
             return q.promise;
         };
 
