f5c8ae0ce98a54b15ccc1027afd96a23534d6189
duyanh <duyanh2005@gmail.com>
2017-02-12 00:52:49 +0700

    - nested component

--- apps/quiz/README.md
+++ apps/quiz/README.md
@@ -5,6 +5,16 @@ open command prompt in quiz folder:
 
 	
 DOM: Document Object Model
-Model + Component = DOM
-Component -> Virtual DOM Component
-Nested Component -> Nested Virtual DOM Component
+	Model + Component = DOM
+	Component -> Virtual DOM Component
+	Nested Component -> Nested Virtual DOM Component
+	
+Render Component:
+	Render a Component means linking it to a DOM element and populate that DOM element
+
+Props are supplied as attributes
+
+To composse component:
+	a component can be nested inside other components
+
+
--- apps/quiz/app/index.js
+++ apps/quiz/app/index.js
@@ -8,17 +8,36 @@ ReactDOM.render(
 /** JSX React.DOM */
 (function(){
     'use strict';
+    var postData = {
+      title: 'React Js',
+      versions:[
+        { name: 'version 1' },
+        { name: 'version 2' },
+        { name: 'version 3' }        
+      ]
+    };
 
-    var Quiz = React.createClass(
-      {
+    var QuizDetail = React.createClass({
         render: function(){
-            return <div>TEST: { this.props.data }</div>
+          return (<div>- Quiz Detail: {this.props.content}</div>);
         }
-      }
-    );
+    });
+
+    var Quiz = React.createClass({
+        render: function(){
+            return (
+              <div>
+                <h3>TEST - Quiz: {this.props.data.title} </h3>                
+                <QuizDetail content={ this.props.data.versions[0].name } />
+                <QuizDetail content={ this.props.data.versions[1].name } />
+                <QuizDetail content={ this.props.data.versions[2].name } />
+              </div>
+            );
+        }
+    });
 
     ReactDOM.render(
-        <Quiz data={"foo"}/>,
+        <Quiz data={postData}></Quiz>,
         document.getElementById('root')
     );
 })();
