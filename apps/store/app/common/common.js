be7e1e7f736ac2936bf8679a487584eee49f69bc
duyanh <duyanh2005@gmail.com>
2017-02-19 13:53:43 +0700

    - convert to components

---
+++ apps/store/app/common/common.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';	
+    angular.module('store.common', [])
+    .factory('appCommon', ['$q', function ($q) {
+        var appCommon = function () {            
+        };
+
+        appCommon.prototype.listInstance = {
+            
+        };        
+
+        return new appCommon();
+    }]);
+})();
+
