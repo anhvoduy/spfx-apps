d86d5c2d5446fc75cd81f5e8830f767eff9ec333
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-03-15 10:42:58 +0700

    - sample data

---
+++ apps/store/app/components/data/baseService.js
@@ -0,0 +1,12 @@
+(function(){
+    'use strict';
+    angular.module('store.data.baseService', [])
+    .factory('baseService', ['$http', '$q', function ($http, $q) {
+        // constructor
+        var baseService = function () {
+            
+        };
+        
+        return baseService();
+	}]);    
+})();
