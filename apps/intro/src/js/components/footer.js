23bba17302bcf13ab3f9e7f774d3cb14ee02ebcd
duyanh <duyanh2005@gmail.com>
2017-05-31 23:14:57 +0700

    - footer.js

--- apps/intro/src/js/components/footer.js
+++ apps/intro/src/js/components/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default class Footer extends React.Component {
 	render() {
 		return(
-			<footer>footer</footer>
+			<footer>{this.props.footer}</footer>
 		);
 	}
 }
