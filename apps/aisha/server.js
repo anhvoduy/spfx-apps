fbcf51a0e4c4acf8d42ea10b85889da65813dd04
duyanh <duyanh2005@gmail.com>
2017-09-23 20:36:14 +0700

    restructure aisha gulp

---
+++ apps/aisha/server.js
@@ -0,0 +1,13 @@
+﻿const express = require('express');
+const http = require('http');
+const path = require("path");
+
+const server = express();
+server.set('port', process.env.PORT || 3000);
+
+server.get('/', function (req, res) {
+    res.send('Hello World!')
+})
+server.use(express.static('public'))
+
+module.exports = server;
