f22eee6291ce9efc1ad22a4bc4e7aafa7daf1e2f
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-09-27 17:21:16 +0700

    update SharePoint WebPart

--- apps/spfx-search-document/src/webparts/spfxSearchDocument/components/SpfxSearchDocument.tsx
+++ apps/spfx-search-document/src/webparts/spfxSearchDocument/components/SpfxSearchDocument.tsx
@@ -40,23 +40,4 @@ export default class SpfxSearchDocument extends React.Component<ISpfxSearchDocum
       </DocumentCard>
     );
   }
-  
-  // public render(): React.ReactElement<ISpfxSearchDocumentProps> {
-  //   return (
-  //     <div className={styles.spfxSearchDocument}>
-  //       <div className={styles.container}>
-  //         <div className={`ms-Grid-row ms-bgColor-themeDark ms-fontColor-white ${styles.row}`}>
-  //           <div className="ms-Grid-col ms-u-lg10 ms-u-xl8 ms-u-xlPush2 ms-u-lgPush1">
-  //             <span className="ms-font-xl ms-fontColor-white">Welcome to SharePoint!</span>
-  //             <p className="ms-font-l ms-fontColor-white">Customize SharePoint experiences using Web Parts.</p>
-  //             <p className="ms-font-l ms-fontColor-white">{escape(this.props.description)}</p>
-  //             <a href="https://aka.ms/spfx" className={styles.button}>
-  //               <span className={styles.label}>Learn more</span>
-  //             </a>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   );
-  // }
 }
