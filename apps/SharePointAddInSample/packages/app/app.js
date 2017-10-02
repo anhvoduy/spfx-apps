48f47dd1d3a3d9c815ed47aa849a17838630f9c0
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-10-02 10:08:49 +0700

    update sampleSearch

--- apps/SharePointAddInSample/packages/app/app.js
+++ apps/SharePointAddInSample/packages/app/app.js
@@ -24,7 +24,7 @@
 				'			<a href="{{i.url}}">															' +
 				'				<img ng-src="{{i.image}}" />												' +
 				'			</a>																			' +
-				'			<p>{{i.DisplayName}}</p>														' +
+				'			<p>{{i.title}}</p>																' +
 				'		</li>																				' +
 				'	</ul>            																		' +
 				'</div>																						';
@@ -40,8 +40,44 @@
     sampleSearchController.$inject = ['$scope'];
     function sampleSearchController($scope){
 		// models
-		$scope.searchString = '';
-		$scope.items = [];
+		$scope.searchString = '';		
+		$scope.items = [
+			{
+				url: 'https://development365.sharepoint.com/Style%20Library/SharePointAddInSample/img/logo.png',
+				title: '50 Must-have plugins for extending Twitter Bootstrap',				
+				image: 'https://development365.sharepoint.com/Style%20Library/SharePointAddInSample/img/logo.png'
+			},
+			{
+				url: 'https://development365.sharepoint.com/Style%20Library/SharePointAddInSample/img/logo.png',
+				title: 'Making a Super Simple Registration System With PHP and MySQL',
+				image: 'https://development365.sharepoint.com/Style%20Library/SharePointAddInSample/img/logo.png'
+			},
+			{
+				url: 'https://development365.sharepoint.com/Style%20Library/SharePointAddInSample/img/logo.png',
+				title: 'Create a slide-out footer with this neat z-index trick',
+				image: 'https://development365.sharepoint.com/Style%20Library/SharePointAddInSample/img/logo.png'
+			},
+			{
+				url: 'https://development365.sharepoint.com/Style%20Library/SharePointAddInSample/img/logo.png',
+				title: 'How to Make a Digital Clock with jQuery and CSS3',
+				image: 'https://development365.sharepoint.com/Style%20Library/SharePointAddInSample/img/logo.png'
+			},
+			{
+				url: 'https://development365.sharepoint.com/Style%20Library/SharePointAddInSample/img/logo.png',
+				title: 'Smooth Diagonal Fade Gallery with CSS3 Transitions',
+				image: 'https://development365.sharepoint.com/Style%20Library/SharePointAddInSample/img/logo.png'
+			},
+			{
+				url: 'https://development365.sharepoint.com/Style%20Library/SharePointAddInSample/img/logo.png',
+				title: 'Mini AJAX File Upload Form',
+				image: 'https://development365.sharepoint.com/Style%20Library/SharePointAddInSample/img/logo.png'
+			},
+			{
+				url: 'https://development365.sharepoint.com/Style%20Library/SharePointAddInSample/img/logo.png',
+				title: 'Your First Backbone.js App – Service Chooser',
+				image: 'https://development365.sharepoint.com/Style%20Library/SharePointAddInSample/img/logo.png'
+			}
+		];
 		
 		// functions
         var activate = function(){
@@ -61,7 +97,7 @@
 			searchString = searchString.toLowerCase();
 			// Using the forEach helper method to loop through the array
 			angular.forEach(arr, function(item){
-				if(item.DisplayName.toLowerCase().indexOf(searchString) !== -1){
+				if(item.title.toLowerCase().indexOf(searchString) !== -1){
 					result.push(item);
 				}
 			});
