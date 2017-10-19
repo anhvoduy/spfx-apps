f134af1f84124f8d0b34fecc7f90597a71f5746f
Anh Vo Duy <anh.voduy@harveynash.vn>
2017-10-19 15:53:14 +0700

    add calendar package

--- apps/SharePointAddInCalendar/packages/app/app.js
+++ apps/SharePointAddInCalendar/packages/app/app.js
@@ -17,20 +17,9 @@
             controller: 'mainCalendarController',			
 			template: function () {
 				var template = 				
-				'	<div id="#instantSearch" class="instantSearch">																				 	       '+
-				'		<div class="bar">        																									 	   '+				
-				'			<input type="text" ng-model="searchString" ng-change="changeSearch(searchString)"                                              '+
-				'                  placeholder="Enter your search terms" ng-keyup="$event.keyCode == 13 && submitSearch(searchString)"/>                   '+
-				'		</div>																														 	   '+
-				'		<ul>        																												 	   '+
-				'			<li ng-repeat="i in items | searchFor:searchString">																	 	   '+
-				'				<a href="{{i.url}}">																								 	   '+
-				'					<img ng-src="{{i.image}}" />																					 	   '+
-				'				</a>																												 	   '+
-				'				<p>{{i.title}}</p>																									 	   '+
-				'			</li>																													 	   '+
-				'		</ul>            																											 	   '+
-				'	</div>																															 	   ';				
+				'<div id="#mainCalendar" class="mainCalendar">  ' +
+				'	Main Calendar								' +
+				'</div>											';				
 				return template;
             },
             link: function (scope, element, attrs, ngCtrl) {
@@ -172,4 +161,95 @@
 			return false;
 		});
 	});	
+})();
+
+
+
+// function generate calendar
+(function(){
+	$(document).ready(function() {		
+		$('#calendar').fullCalendar({
+			header: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'month,agendaWeek,agendaDay'
+			},
+			defaultDate: '2017-10-12',
+			navLinks: true, // can click day/week names to navigate views
+			selectable: true,
+			selectHelper: true,
+			select: function(start, end) {
+				var title = prompt('Event Title:');
+				var eventData;
+				if (title) {
+					eventData = {
+						title: title,
+						start: start,
+						end: end
+					};
+					$('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+				}
+				$('#calendar').fullCalendar('unselect');
+			},
+			editable: true,
+			eventLimit: true, // allow "more" link when too many events
+			events: [
+				{
+					title: 'All Day Event',
+					start: '2017-10-01'
+				},
+				{
+					title: 'Long Event',
+					start: '2017-10-07',
+					end: '2017-10-10'
+				},
+				{
+					id: 999,
+					title: 'Repeating Event',
+					start: '2017-10-09T16:00:00'
+				},
+				{
+					id: 999,
+					title: 'Repeating Event',
+					start: '2017-10-16T16:00:00'
+				},
+				{
+					title: 'Conference',
+					start: '2017-10-11',
+					end: '2017-10-13'
+				},
+				{
+					title: 'Meeting',
+					start: '2017-10-12T10:30:00',
+					end: '2017-10-12T12:30:00'
+				},
+				{
+					title: 'Lunch',
+					start: '2017-10-12T12:00:00'
+				},
+				{
+					title: 'Meeting',
+					start: '2017-10-12T14:30:00'
+				},
+				{
+					title: 'Happy Hour',
+					start: '2017-10-12T17:30:00'
+				},
+				{
+					title: 'Dinner',
+					start: '2017-10-12T20:00:00'
+				},
+				{
+					title: 'Birthday Party',
+					start: '2017-10-13T07:00:00'
+				},
+				{
+					title: 'Click for Google',
+					url: 'http://google.com/',
+					start: '2017-10-28'
+				}
+			]
+		});
+		
+	});
 })();
